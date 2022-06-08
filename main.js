c019dedaf3dcd5c9ec388c1ee50f3dc06856187a
Roma Bilibov <romabilibov@gmail.com>
2022-06-08 23:12:20 +0200

    Fixed close process

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isLinux = process.platform === 'linux' ? true : false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isLinux = process.platform === "linux" ? true : false;
 
-console.log(process.platform)
+console.log(process.platform);
 
-let mainWindow
+let mainWindow;
 
 const createMainWindow = () => {
-    mainWindow = new BrowserWindow({
-        title: 'ImageShrink',
-        width: 500,
-        height: 600,
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-        resizable: isDev ? true : false
-    })
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
+  });
 
-    //mainWindow.loadURL('https://twitter.com')
-    
-    //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  //mainWindow.loadURL('https://twitter.com')
 
-    mainWindow.loadFile(`${__dirname}/app/index.html`)
-}
+  //mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
-app.on('ready', createMainWindow)
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
